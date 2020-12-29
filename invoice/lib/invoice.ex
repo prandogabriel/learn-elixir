9a4cd7e8d9ca7253dec99d46bb905fed3579b809
Gabriel <gprando55@gmail.com>
2020-12-29 17:19:25 -0300

    chore: add functions and tests :zap:

--- invoice/lib/invoice.ex
+++ invoice/lib/invoice.ex
@@ -1,5 +1,39 @@
 defmodule Invoice do
-  def hello do
-    "Hellow world"
+  @moduledoc """
+    Funções de invoices
+  """
+
+  @doc """
+    Ao receber `invoice` retorna um array de invoices
+      ## Exemplos
+      iex> Invoice.create_invoices(["agua", "luz", "comida"])
+      ["agua", "luz", "comida"]    
+  """
+
+  def create_invoices(invoices, _pay) do
+    for invoice <- invoices do
+      invoice
+    end
   end
-end
+
+  @doc """
+    Ao receber uma lista de `invoices`, deve ordenálas
+      ## Exemplos
+      iex> Invoice.sort_invoices(Invoice.create_invoices(["agua", "luz", "comida"]))
+      ["agua", "comida", "luz"] 
+  """
+  def sort_invoices(invoices ) do
+    Enum.sort(invoices)
+  end
+
+  @doc """
+    Ao receber `invoices` e um elemento `invoice` retorna se existe esse invoice nos invoices
+      ## Exemplo
+      iex> Invoice.invoice_exists?(Invoice.create_invoices(["agua", "luz", "comida"]), "luz")
+      true
+  """
+
+  def invoice_exists?(invoices, invoice) do
+    Enum.member?(invoices, invoice)
+  end
+end 
--- invoice/test/invoice_test.exs
+++ invoice/test/invoice_test.exs
@@ -2,7 +2,18 @@ defmodule InvoiceTest do
   use ExUnit.Case
   doctest Invoice
 
-  test "greets the world" do
-    assert Invoice.hello() == :world
+  test "deve criar uma lista de faturas" do
+    invoices = Invoice.create_invoices(["food", "sport"], "")
+    assert invoices == ["food", "sport"]
+  end
+
+  test "deve ordenar uma lista de faturas" do
+    invoices = Invoice.create_invoices(["sport", "food"], "")
+    refute Invoice.sort_invoices(invoices) == ["sport", "food"]
+  end
+
+  test "deve verificar se uma fatura existe em uma lista de faturas" do
+    invoices = Invoice.create_invoices(["sport", "food"], "")
+    assert  Invoice.invoice_exists?(invoices, "sport") == true
   end
 end
